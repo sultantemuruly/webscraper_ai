5df77fe6c594b6e6bf92161856edf1331662d316
sultantemuruly <sultantemuruly@gmail.com>
2025-02-07 17:02:59 +0500

    Web Scraping with Selenium

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from scrape import scrape_website
 
 st.title("Web Scraper AI")
 url = st.text_input("Enter a website URL")
 
 if st.button("Scrape Site"):
     st.write("Button is pressed")
+    result = scrape_website(url)
+    print(result)
---
+++ scrape.py
@@ -0,0 +1,19 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+def scrape_website(website):
+    print("Launching chrome browser...")
+
+    chrome_driver_path = "./chromedriver"
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        driver.get(website)
+        print("Page loaded...")
+        html = driver.page_source
+        return html
+    finally:
+        driver.quit()
